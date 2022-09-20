604ef62b8fbe8a4c1e78e0ca9fc7be3feb72fb7f
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-20 23:03:23 +0200

    added TodoComponent to the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule, FormsModule
